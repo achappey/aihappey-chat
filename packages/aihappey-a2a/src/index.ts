080157d96e79a171dd96f8b6dfe1b67d9a6fe95c
Arthur Bleij <github@achappey.nl>
2026-07-22 17:23:47 +0200

    Bump @a2a-js/sdk to 1.0.0 and trim exported A2A types

--- packages/aihappey-a2a/src/index.ts
+++ packages/aihappey-a2a/src/index.ts
@@ -3,17 +3,12 @@ export * from './AuthA2AClient';
 export {
  // A2AClient,
   Message,
-  MessageSendParams,
   Task,
   Artifact,
   AgentCard,
   AgentSkill,
   AgentExtension,
-  TaskQueryParams,
   SendMessageResponse,
-  GetTaskResponse,
-  SendMessageSuccessResponse,
-  GetTaskSuccessResponse
 } from "@a2a-js/sdk";
 
 //const client = new A2AClient("http://localhost:41241");
